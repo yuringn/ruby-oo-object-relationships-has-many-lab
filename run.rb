fd36e855f82036daa679249c83a62ea8bb1cce5c
Yuri Nguyen <yuringuyen312@gmail.com>
2021-02-28 15:36:19 -0500

    commit tree 6b61604f380b7bb52be74f0209b196d1d4354ee1

---
+++ run.rb
@@ -0,0 +1,21 @@
+require_relative"lib/artist"
+require_relative"lib/song"
+
+require"pry"
+
+
+adele = Artist.new("Adele")
+beyonce = Artist.new("Beyonce")
+
+# adele.song = "Hello"
+# beyonce.song = "Say My Name"
+
+hello = Song.new("Hello")
+alliask = Song.new("All I Ask")
+saymyname = Song.new("Say My Name")
+
+
+
+
+
+binding.pry
